e4b7e362062f189686c4c4b501a64e226b5e3935
Kenny Du <kennydus@gmail.com>
2021-12-06 13:08:42 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/MegaWar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MegaWar.dir/main.cpp.obj"
+  "MegaWar.exe"
+  "MegaWar.exe.manifest"
+  "MegaWar.lib"
+  "MegaWar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MegaWar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/MegaWar.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MegaWar.
+# This may be replaced when dependencies are built.
